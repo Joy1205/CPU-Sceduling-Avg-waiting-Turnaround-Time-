72d7fc921e00281ed17c0b98ca395a9500358d4d
Joy Chandra Talukder <57414994+Joy1205@users.noreply.github.com>
2020-03-23 22:21:26 +0530

    Update CPU Sceduling.c

--- CPU Sceduling.c
+++ CPU Sceduling.c
@@ -43,7 +43,7 @@ time+=tq;
 if(rt[count]==0 && flag==0)
 {
 remaining--;
-printf("P[%d]\t%d\t%d\n",count+1.time-art[count].time-art[count]-bt[count]);
+printf("P[%d]\t%d\t%d\n",count+1,time-art[count],time-art[count]-bt[count]);
 wt+=time-art[count]-bt[count];
 tart+=time-art[count];
 flag=0;
